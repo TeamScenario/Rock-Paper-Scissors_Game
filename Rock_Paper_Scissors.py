b08d195aa518e0ea8cbeca7d4ce737dd845bffdc
Snehashish Biswas <teamscenariox@gmail.com>
2022-12-30 17:41:31 +0530

    Less lines

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -1,21 +1,11 @@
 ''' By @CoderX on Telegram'''
 
-import random
-
-def RPS(user_input):
-    choices = ["Rock", "Paper", "Scissors"]
-    res = random.choice(choices)
-    U_W = "Computer's input:- {}\nYour input:- {}\n\nResult :- ✨ You won ✨".format(res,user_input)
-    C_W = "Computer's input:- {}\nYour input:- {}\n\nResult :- 😴 Computer won 😴".format(res,user_input)
-    N_W = "Computer's input:- {}\nYour input:- {}\n\nResult :- it's a tie!".format(res,user_input)
-    
-    if user_input == res:
-        return N_W
-
-    elif user_input == "Rock" and res == "Scissors" or user_input == "Paper" and res == "Rock" or user_input == "Scissors" and res == "Paper":
-        return U_W
-                
-    elif user_input == "Rock" and res == "Paper" or user_input == "Paper" and res == "Scissors" or user_input == "Scissors" and res == "Rock":
-        return C_W
-    else:
-        return "wrong input"
+def RPS():
+    user_input, choices = input("Enter your input\n    • Rock\n    • Paper\n    • Scissors\n    • 0 to exit:  ").title(), ["Rock", "Paper", "Scissors"] 
+    res = __import__("random").choice(choices)
+    WHW = ["Computer's input:- {}\nYour input:- {}\n\nResult :- ✨ You won ✨".format(res,user_input), "Computer's input:- {}\nYour input:- {}\n\nResult :- 😴 Computer won 😴".format(res,user_input), "Computer's input:- {}\nYour input:- {}\n\nResult :- it's a tie!".format(res,user_input)]
+    if user_input == res:return WHW[2]
+    elif user_input == "Rock" and res == "Scissors" or user_input == "Paper" and res == "Rock" or user_input == "Scissors" and res == "Paper":return WHW[0]
+    elif user_input == "Rock" and res == "Paper" or user_input == "Paper" and res == "Scissors" or user_input == "Scissors" and res == "Rock":return WHW[1]
+    elif user_input == "0":exit("\033[H\033[2J\033[32m\033[1mEnded\033[0m")
+    else:return "wrong input"
